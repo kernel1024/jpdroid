487f0fdbdbd18d2d77abdee0778fdb738fe53329
kernel1024 <kernel1024@users.noreply.github.com>
2014-03-06 15:07:35 +0000

    Interop.

--- app/src/main/java/AuxTranslator.java
+++ app/src/main/java/AuxTranslator.java
@@ -1,8 +1,8 @@
-import com.tscorp.jpdroid.app.AtlasException;
-
-import java.io.IOException;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class AuxTranslator implements Runnable {
 
@@ -11,50 +11,69 @@ public class AuxTranslator implements Runnable {
     private int atl_port;
     private String atl_host;
     private String atl_text;
+    private Handler ui_handler;
 
-    AuxTranslator(String hostname, int port, int timeout, int retries, String text) {
+    AuxTranslator(String hostname, int port, int timeout, int retries, String text, Handler uiHandler) {
         atl_host = hostname;
         atl_port = port;
         atl_timeout = timeout;
         atl_retries = retries;
         atl_text = text;
+        ui_handler = uiHandler;
     }
 
-    private void sendAlertMsg(String msg) {
-
+    private void sendProgressMsg(int progress) {
+        Message msg = ui_handler.obtainMessage();
+        Bundle b = new Bundle();
+        b.putInt("progress",progress);
+        msg.setData(b);
+        ui_handler.sendMessage(msg);
     }
 
-    private void sendReadyText(List<String> text) {
-
+    private void sendAlertMsg(String s) {
+        Message msg = ui_handler.obtainMessage();
+        Bundle b = new Bundle();
+        b.putString("message", s);
+        msg.setData(b);
+        ui_handler.sendMessage(msg);
     }
 
-    // thread examples - http://arashmd.blogspot.de/2013/06/java-threading.html
+    private void sendReadyText(ArrayList<String> text) {
+        Message msg = ui_handler.obtainMessage();
+        Bundle b = new Bundle();
+        b.putStringArrayList("text", text);
+        msg.setData(b);
+        ui_handler.sendMessage(msg);
+    }
 
     @Override
     public void run() {
-        List<String> tx = Arrays.asList(atl_text.split("\\r?\\n"));
-        List<String> xt = Arrays.asList();
+        ArrayList<String> tx = new ArrayList<String>(Arrays.asList(atl_text.split("\\r?\\n")));
+        ArrayList<String> xt = new ArrayList<String>();
 
         AtlasTranslator tran = new AtlasTranslator(atl_timeout);
         try {
             boolean ok_tran = false;
             for (int i=0;i<atl_retries;i++) {
                 if (!tran.initTran(atl_host,atl_port)) continue;
+                sendProgressMsg(0);
                 for (String txs : tx) {
                     xt.add(tran.tranString(txs));
                     ok_tran = true;
+                    sendProgressMsg(100*tx.indexOf(txs)/txs.length());
                 }
+                sendProgressMsg(100);
             }
             if (!ok_tran) {
-                xt.clear();
                 sendAlertMsg("ATLAS connection failure");
             } else if (!xt.isEmpty()) {
                 sendReadyText(xt);
             }
-        } catch (IOException e) {
-            xt.clear(); sendAlertMsg(e.getMessage());
-        } catch (AtlasException e) {
-            xt.clear(); sendAlertMsg(e.getMessage());
+        } catch (Exception e) {
+            sendAlertMsg(e.getMessage());
+        } finally {
+            xt.clear();
+            sendProgressMsg(-1);
         }
 
     }
