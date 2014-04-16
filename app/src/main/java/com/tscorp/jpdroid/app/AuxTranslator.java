67cd6873f600aef92005306238b1e6ad0aedd7af
kernel1024 <kernel1024@users.noreply.github.com>
2014-04-16 10:55:13 +0000

    Advanced connections retry code, stop action for translation thread interruption.

--- app/src/main/java/com/tscorp/jpdroid/app/AuxTranslator.java
+++ app/src/main/java/com/tscorp/jpdroid/app/AuxTranslator.java
@@ -21,6 +21,7 @@ class AuxTranslator implements Runnable {
     private final OutputMode out_mode;
     private ArrayList<String> atl_text;
     private Handler ui_handler;
+    private boolean stopTranslation;
 
     AuxTranslator(String hostname, int port, int timeout, int retries, ArrayList<String> text,
                   Handler uiHandler, OutputMode outMode) {
@@ -33,6 +34,14 @@ class AuxTranslator implements Runnable {
         out_mode = outMode;
     }
 
+    private void sendMessageWithString(String key, String value) {
+        Message msg = ui_handler.obtainMessage();
+        Bundle b = new Bundle();
+        b.putString(key, value);
+        msg.setData(b);
+        ui_handler.sendMessage(msg);
+    }
+
     private void sendProgressMsg(int progress) {
         Message msg = ui_handler.obtainMessage();
         Bundle b = new Bundle();
@@ -42,92 +51,90 @@ class AuxTranslator implements Runnable {
     }
 
     private void sendAlertMsg(String s) {
-        Message msg = ui_handler.obtainMessage();
-        Bundle b = new Bundle();
-        b.putString("message", s);
-        msg.setData(b);
-        ui_handler.sendMessage(msg);
+        sendMessageWithString("message",s);
     }
 
     private void sendReadyText(String text) {
-        Message msg = ui_handler.obtainMessage();
-        Bundle b = new Bundle();
-        b.putString("text", text);
-        msg.setData(b);
-        ui_handler.sendMessage(msg);
+        sendMessageWithString("text", text);
     }
 
     private void sendReadyTextToast(String text) {
-        Message msg = ui_handler.obtainMessage();
-        Bundle b = new Bundle();
-        b.putString("toast", text);
-        msg.setData(b);
-        ui_handler.sendMessage(msg);
+        sendMessageWithString("toast", text);
+    }
+
+    private void sendCleanupRequest() {
+        sendMessageWithString("cleanup", "");
+    }
+
+    public void breakTranslation() {
+        stopTranslation = true;
     }
 
     @Override
     public void run() {
+        stopTranslation = false;
         String res = "";
         String a_msg = "";
         String lastTran = "";
-        boolean fTran = (out_mode == OutputMode.TOAST);
+        boolean simpleTran = (out_mode == OutputMode.TOAST);
 
-        AtlasTranslator tran = new AtlasTranslator(atl_timeout);
-        boolean ok_conn = false;
         try {
             String c_msg = "";
+            boolean tranComplete = false;
+            sendAlertMsg("Connecting...");
             for (int i = 0; i < atl_retries; i++) {
-                boolean ti = false;
+                AtlasTranslator tran = new AtlasTranslator(atl_timeout);
                 try {
-                    ti = tran.initTran(atl_host, atl_port);
+                    if (tran.isConnected())
+                        tran.doneTran();
+                    if (tran.initTran(atl_host, atl_port)) {
+                        if (!simpleTran)
+                            sendProgressMsg(0);
+                        int idx = 0;
+                        for (String txs : atl_text) {
+                            if (stopTranslation) break;
+                            String s = tran.tranString(txs);
+                            lastTran = s;
+                            res += txs + "\n" + s + "\n\n";
+                            idx++;
+                            if (!simpleTran)
+                                sendProgressMsg(100 * idx / atl_text.size());
+                        }
+                        if (!simpleTran)
+                            sendProgressMsg(100);
+                        tranComplete = true;
+                    }
+                    tran.doneTran();
                 } catch (Exception e) {
+                    if (tran.isConnected())
+                        tran.doneTran();
                     c_msg = e.getMessage();
                     Thread.sleep(atl_timeout * 1000);
                 }
-                if (!ti) continue;
-                ok_conn = true;
-                break;
+                if (tranComplete) break;
             }
-            if (!ok_conn) {
+            if (!tranComplete) {
                 if (c_msg.isEmpty())
-                    c_msg = "ATLAS connection failure";
+                    c_msg = "ATLAS initialization failure";
                 throw new AtlasException(c_msg);
             }
 
-            if (!fTran)
-                sendProgressMsg(0);
-            int idx = 0;
-            for (String txs : atl_text) {
-                String s = tran.tranString(txs);
-                lastTran = s;
-                res += txs + "\n" + s + "\n\n";
-                idx++;
-                if (!fTran)
-                    sendProgressMsg(100 * idx / atl_text.size());
-            }
-            if (!fTran)
-                sendProgressMsg(100);
-            tran.doneTran();
-
         } catch (Exception e) {
             a_msg = e.getMessage();
-            try {
-                if (ok_conn) tran.doneTran();
-            } catch (Exception ex) {
-                Log.e("AuxTranslator", "Double exception while closing ATLAS", ex);
-            }
         }
         if (!a_msg.isEmpty())
             sendAlertMsg(a_msg);
         else if (!res.isEmpty()) {
-            if (fTran)
+            if (simpleTran)
                 sendReadyTextToast(lastTran);
             else
                 sendReadyText(res);
         } else
             sendAlertMsg("Empty result");
 
-        if (!fTran)
+        if (!simpleTran)
             sendProgressMsg(-1);
+
+        sendCleanupRequest();
     }
 }
